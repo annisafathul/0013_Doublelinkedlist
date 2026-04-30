e820e379f525172af97b4198e316ff46e3ef129c
annisafathul <annisafathul18@gmail.com>
2026-04-30 15:47:03 +0700

    Add a void function to the addNode

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -22,4 +22,12 @@ class DoubleLinkedList
         START = NULL;
     }
 
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
+
+
 }
